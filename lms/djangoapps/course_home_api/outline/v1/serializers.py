e81a69cddc63a0402b4a7c5012a85cfb2f781a64
Carla Duarte <cduarte@edx.org>
2020-06-09 18:26:32 -0400

    AA-181: Updating Outline Tab API to include Course Blocks

--- lms/djangoapps/course_home_api/outline/v1/serializers.py
+++ lms/djangoapps/course_home_api/outline/v1/serializers.py
@@ -2,8 +2,8 @@
 Outline Tab Serializers.
 """
 
-
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 
 class CourseToolSerializer(serializers.Serializer):
@@ -21,8 +21,35 @@ class CourseToolSerializer(serializers.Serializer):
         return request.build_absolute_uri(url)
 
 
+class CourseBlockSerializer(serializers.Serializer):
+    """
+    Serializer for Course Block Objects
+    """
+    blocks = serializers.SerializerMethodField()
+
+    def get_blocks(self, blocks):
+        data = {}
+
+        for block_key in blocks:
+            block_data = {
+                'id': str(block_key),
+                'type': str(block_key.category),
+                'display_name': blocks.get_xblock_field(block_key, 'display_name', str(block_key.category)),
+                'lms_web_url': reverse(
+                    'jump_to',
+                    kwargs={'course_id': str(block_key.course_key), 'location': str(block_key)},
+                    request=self.context['request'],
+                ),
+                'children': [str(child) for child in blocks.get_children(block_key)],
+            }
+            data[str(block_key)] = block_data
+
+        return data
+
+
 class OutlineTabSerializer(serializers.Serializer):
     """
     Serializer for the Outline Tab
     """
     course_tools = CourseToolSerializer(many=True)
+    course_blocks = CourseBlockSerializer()
